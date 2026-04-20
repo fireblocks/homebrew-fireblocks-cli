7833f7d1736bd9396095d9585692ae9200a62589
Eran Abir <108397990+eranfireblocks@users.noreply.github.com>
2026-04-20 18:03:24 +0300

    Update fireblocks-cli to 1.0.2 (#1)
    [FIREHOG-BYPASS] sha256 meant to be public

---
+++ Formula/fireblocks-cli.rb
@@ -0,0 +1,30 @@
+class FireblocksCli < Formula                                        
+    desc "Agent-first CLI for Fireblocks infrastructure"
+    homepage "https://github.com/fireblocks/fireblocks-cli"            
+    version "1.0.2"                                                    
+    license "MIT"                                                      
+                                                                       
+    on_macos do                                                        
+      if Hardware::CPU.arm?
+        url "https://github.com/fireblocks/fireblocks-cli/releases/down
+  load/v1.0.2/fireblocks-v1.0.2-9416c27-darwin-arm64.tar.gz"           
+        sha256
+  "721ea2c1d733996203e2d2f6a49af0d6c888d6a3261a56291239ebc9c9c5a536"   
+      else        
+        url "https://github.com/fireblocks/fireblocks-cli/releases/down
+  load/v1.0.2/fireblocks-v1.0.2-9416c27-darwin-x64.tar.gz"
+        sha256
+  "7be59eab2e6668f8eb7edcd4df98e73c33aada3085af2dec19f9f8db2b979670"   
+      end
+    end                                                                
+                  
+    def install
+      libexec.install Dir["*"]
+      bin.write_exec_script libexec/"bin/fireblocks"
+    end                                                                
+   
+    test do                                                            
+      assert_match version.to_s, shell_output("#{bin}/fireblocks
+  --version")
+    end
+  end
