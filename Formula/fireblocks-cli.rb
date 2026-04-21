debd2e363ddd8b9669b2d0f1d1316a372dbc86a1
eran <eran@fireblocks.com>
2026-04-21 14:02:21 +0300

    Update fireblocks-cli to 1.0.5
    [FIREHOG-BYPASS] y

--- Formula/fireblocks-cli.rb
+++ Formula/fireblocks-cli.rb
@@ -1,19 +1,19 @@
 class FireblocksCli < Formula
   desc "Command-line interface for Fireblocks infrastructure"
   homepage "https://github.com/fireblocks/fireblocks-cli"
-  version "1.0.3"
+  version "1.0.5"
   license "MIT"
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/fireblocks/fireblocks-cli/releases/download/v1.0.3/fireblocks-v1.0.3-3d5771d-darwin-arm64.tar.gz"
-      sha256 "06ca22893bbad5ee72d5a9f5931b75f05e509392b88e26dd8cba91b30fe21048"
+      url "https://github.com/fireblocks/fireblocks-cli/releases/download/v1.0.5/fireblocks-v1.0.5-f038846-darwin-arm64.tar.gz"
+      sha256 "df6b41f156dc20a49149bdabe51638a9695b777495804e14eaa6f8c636e18c99"
     end
     on_intel do
-      url "https://github.com/fireblocks/fireblocks-cli/releases/download/v1.0.3/fireblocks-v1.0.3-3d5771d-darwin-x64.tar.gz"
-      sha256 " c3dde6e416cfd5406b7ad69302a0dab0374e757a75fc1e8064d8c7b4631d197c"
+      url "https://github.com/fireblocks/fireblocks-cli/releases/download/v1.0.5/fireblocks-v1.0.5-f038846-darwin-x64.tar.gz"
+      sha256 "980ba77279f9af450f1f365e1e2a7df9fc378ced3b809fc74203a5ea49fc31ef"
     end
   end
 
